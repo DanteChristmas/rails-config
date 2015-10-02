e8c8ef49581c92c4d965c918aeeaa98f5d8ccc87
Josh Sullivan <jsullivan@silverchalice.com>
2015-10-02 13:24:03 -0600

    Cleaning up the accounts index page, made the account-table directive

---
+++ oas_config/app/assets/javascripts/directives/accountTable.js
@@ -0,0 +1,40 @@
+'use strict';
+
+angular.module('oasConfig')
+  .directive('accountTable', ['$location', function($location) {
+    return {
+      restrict: 'E',
+      templateUrl: 'assets/templates/directives/account-table.html',
+      replace: true,
+      scope: {
+        accounts: '='
+      },
+      link: function(scope, element) {
+
+        var _helper ={
+        };
+
+
+        var link = {
+          init: function(){
+            link.setScope();
+          },
+
+          setScope: function(){
+            for(var property in link.scope){
+              scope[property] = link.scope[property];
+            }
+          },
+
+          scope: {
+
+          }
+        };
+
+
+        link.init();
+
+      }
+    };
+  }
+]);
